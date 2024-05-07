7b0a42d23e9e1e275697f7533452b136872f8d13
GwenTogni <gwen.togni@student.supsi.ch>
2024-05-07 21:52:02 +0200

    Add island

--- src_map/app.js
+++ src_map/app.js
@@ -1,82 +1,191 @@
 
 function leaveDescription(){
-  document.getElementById('description').style.display = "none"
+  document.getElementById('descriptionCity').style.display = "none";
+  document.getElementById('descriptionPlace').style.display = "none";
+  document.getElementById('descriptionRoute').style.display = "none";
+  document.getElementById('descriptionIsland').style.display = "none";
 }
 
 /* Text description city */
 function city001(){
-  document.getElementById('description').style.display = "block";
-  document.getElementById('title').innerHTML = 'IMPRESA DEFORESTAZIONE';
-  document.getElementById('generic').innerHTML = 'GESTITA IN MANIERA CONSAPEVOLE.';
+  document.getElementById('descriptionCity').style.display = "block";
+  document.getElementById('descriptionPlace').style.display = "none";
+  document.getElementById('descriptionRoute').style.display = "none";
+  document.getElementById('descriptionIsland').style.display = "none";
+  document.getElementById('titleCity').innerHTML = 'IMPRESA DEFORESTAZIONE CONSAPEVOLE';
+  document.getElementById('textCity').innerHTML = '';
 }
 function city002(){
-  document.getElementById('description').style.display = "block";
-  document.getElementById('title').innerHTML = 'AEREOPORTO GREEN AIR';
-  document.getElementById('generic').innerHTML = 'VOLI GREEN E LOW-COST VERSO TUTTO IL MONDO!';
+  document.getElementById('descriptionCity').style.display = "block";
+  document.getElementById('descriptionPlace').style.display = "none";
+  document.getElementById('descriptionRoute').style.display = "none";
+  document.getElementById('descriptionIsland').style.display = "none";
+  document.getElementById('titleCity').innerHTML = 'AEREOPORTO GREEN AIR';
+  document.getElementById('textCity').innerHTML = 'VOLI GREEN E LOW-COST VERSO TUTTO IL MONDO!';
 }
 function city003(){
-  document.getElementById('description').style.display = "block";
-  document.getElementById('title').innerHTML = 'DITTA FAST FASHION';
-  document.getElementById('generic').innerHTML = 'LOCAL & GREEN';
+  document.getElementById('descriptionCity').style.display = "block";
+  document.getElementById('descriptionPlace').style.display = "none";
+  document.getElementById('descriptionRoute').style.display = "none";
+  document.getElementById('descriptionIsland').style.display = "none";
+  document.getElementById('titleCity').innerHTML = 'DITTA FAST FASHION LOCAL & GREEN';
+  document.getElementById('textCity').innerHTML = '';
 }
 function city004(){
-  document.getElementById('description').style.display = "block";
-  document.getElementById('title').innerHTML = 'FABBRICA DI ALU';
-  document.getElementById('generic').innerHTML = ' 100% RICICLABILE';
+  document.getElementById('descriptionCity').style.display = "block";
+  document.getElementById('descriptionPlace').style.display = "none";
+  document.getElementById('descriptionRoute').style.display = "none";
+  document.getElementById('descriptionIsland').style.display = "none";
+  document.getElementById('titleCity').innerHTML = 'FABBRICA DI ALU 100% RICICLABILE';
+  document.getElementById('textCity').innerHTML = '';
 }
 function city005(){
-  document.getElementById('description').style.display = "block";
-  document.getElementById('title').innerHTML = 'MACELLO';
-  document.getElementById('generic').innerHTML = 'ECO-FRIENDLY';
+  document.getElementById('descriptionCity').style.display = "block";
+  document.getElementById('descriptionPlace').style.display = "none";
+  document.getElementById('descriptionRoute').style.display = "none";
+  document.getElementById('descriptionIsland').style.display = "none";
+  document.getElementById('titleCity').innerHTML = 'MACELLO ECO-FRIENDLY';
+  document.getElementById('textCity').innerHTML = '';
 }
 function city006(){
-  document.getElementById('description').style.display = "block";
-  document.getElementById('title').innerHTML = 'LABORATORIO VIVISEZIONE ETICA';
-  document.getElementById('generic').innerHTML = '';
+  document.getElementById('descriptionCity').style.display = "block";
+  document.getElementById('descriptionPlace').style.display = "none";
+  document.getElementById('descriptionRoute').style.display = "none";
+  document.getElementById('descriptionIsland').style.display = "none";
+  document.getElementById('titleCity').innerHTML = 'LABORATORIO VIVISEZIONE ETICA';
+  document.getElementById('textCity').innerHTML = '';
 }
 function city007(){
-  document.getElementById('description').style.display = "block";
-  document.getElementById('title').innerHTML = 'INCENERITORE ARTIGIANALE';
-  document.getElementById('generic').innerHTML = '';
+  document.getElementById('descriptionCity').style.display = "block";
+  document.getElementById('descriptionPlace').style.display = "none";
+  document.getElementById('descriptionRoute').style.display = "none";
+  document.getElementById('descriptionIsland').style.display = "none";
+  document.getElementById('titleCity').innerHTML = 'INCENERITORE ARTIGIANALE';
+  document.getElementById('textCity').innerHTML = '';
 }
 function city008(){
-  document.getElementById('description').style.display = "block";
-  document.getElementById('title').innerHTML = 'FONTE DELLA VITA ETERNIT';
-  document.getElementById('generic').innerHTML = '';
+  document.getElementById('descriptionCity').style.display = "block";
+  document.getElementById('descriptionPlace').style.display = "none";
+  document.getElementById('descriptionRoute').style.display = "none";
+  document.getElementById('descriptionIsland').style.display = "none";
+  document.getElementById('titleCity').innerHTML = 'VILLAGGIO FONTE DELLA VITA ETERNIT';
+  document.getElementById('textCity').innerHTML = '';
+}
+
+function city009(){
+  document.getElementById('descriptionCity').style.display = "block";
+  document.getElementById('descriptionPlace').style.display = "none";
+  document.getElementById('descriptionRoute').style.display = "none";
+  document.getElementById('descriptionIsland').style.display = "none";
+  document.getElementById('titleCity').innerHTML = 'PORTO DEI VANDALI AMBIENTALISTI';
+  document.getElementById('textCity').innerHTML = '';
+}
+function city010(){
+  document.getElementById('descriptionCity').style.display = "block";
+  document.getElementById('descriptionPlace').style.display = "none";
+  document.getElementById('descriptionRoute').style.display = "none";
+  document.getElementById('titleCity').innerHTML = 'INDUSTRIE ALIMENTI SINTETICI OGM';
+  document.getElementById('textCity').innerHTML = '(RISO VEGANO, PASTA VEGANA, MARMELLATA VEGANA)';
+}
+function city011(){
+  document.getElementById('descriptionCity').style.display = "block";
+  document.getElementById('descriptionPlace').style.display = "none";
+  document.getElementById('descriptionRoute').style.display = "none";
+  document.getElementById('descriptionIsland').style.display = "none";
+  document.getElementById('titleCity').innerHTML = 'VILLAGGIO TOFU';
+  document.getElementById('textCity').innerHTML = '';
+}
+function city012(){
+  document.getElementById('descriptionCity').style.display = "block";
+  document.getElementById('descriptionPlace').style.display = "none";
+  document.getElementById('descriptionRoute').style.display = "none";
+  document.getElementById('descriptionIsland').style.display = "none";
+  document.getElementById('titleCity').innerHTML = 'VILLAGGIO HIPPIE';
+  document.getElementById('textCity').innerHTML = '';
 }
 
 /* Text description route */
 function route001(){
-  document.getElementById('description').style.display = "block";
-  document.getElementById('title').innerHTML = 'TRATTA DEGLI SCHIAVI';
-  document.getElementById('generic').innerHTML = 'PER IMPORTARE CIBO VEGANO.';
+  document.getElementById('descriptionCity').style.display = "none";
+  document.getElementById('descriptionPlace').style.display = "none";
+  document.getElementById('descriptionIsland').style.display = "none";
+  document.getElementById('descriptionRoute').style.display = "block";
+  document.getElementById('titleRoute').innerHTML = 'TRATTA DEGLI SCHIAVI PER IMPORTARE CIBO VEGANO';
+  document.getElementById('textRoute').innerHTML = '';
 }
 function route002(){
-  document.getElementById('description').style.display = "block";
-  document.getElementById('title').innerHTML = 'AUTOSTRADA';
-  document.getElementById('generic').innerHTML = 'IN ECO-ASFALTO';
+  document.getElementById('descriptionCity').style.display = "none";
+  document.getElementById('descriptionPlace').style.display = "none";
+  document.getElementById('descriptionIsland').style.display = "none";
+  document.getElementById('descriptionRoute').style.display = "block";
+  document.getElementById('titleRoute').innerHTML = 'AUTOSTRADA IN ECO-ASFALTO';
+  document.getElementById('textRoute').innerHTML = '';
 }
 
 /* Text description places */
 function place001(){
-  document.getElementById('description').style.display = "block";
-  document.getElementById('title').innerHTML = 'MONTE CENERI';
-  document.getElementById('generic').innerHTML = '';
+  document.getElementById('descriptionPlace').style.display = "block";
+  document.getElementById('descriptionCity').style.display = "none";
+  document.getElementById('descriptionIsland').style.display = "none";
+  document.getElementById('descriptionRoute').style.display = "none";
+  document.getElementById('titlePlace').innerHTML = 'ESTRATTORI DI PETROLIO 100% NATURALE';
+  document.getElementById('textPlace').innerHTML = '';
+}
+function place002(){
+  document.getElementById('descriptionPlace').style.display = "block";
+  document.getElementById('descriptionCity').style.display = "none";
+  document.getElementById('descriptionIsland').style.display = "none";
+  document.getElementById('descriptionRoute').style.display = "none";
+  document.getElementById('titlePlace').innerHTML = 'DISCARICA AL Km 0';
+  document.getElementById('textPlace').innerHTML = '';
+}
+function place003(){
+  document.getElementById('descriptionPlace').style.display = "block";
+  document.getElementById('descriptionCity').style.display = "none";
+  document.getElementById('descriptionIsland').style.display = "none";
+  document.getElementById('descriptionRoute').style.display = "none";
+  document.getElementById('titlePlace').innerHTML = 'ALPI DELLE MUCCHE FELICI';
+  document.getElementById('textPlace').innerHTML = '';
+}
+function place004(){
+  document.getElementById('descriptionPlace').style.display = "block";
+  document.getElementById('descriptionCity').style.display = "none";
+  document.getElementById('descriptionIsland').style.display = "none";
+  document.getElementById('descriptionRoute').style.display = "none";
+  document.getElementById('titlePlace').innerHTML = 'COLTIVAZIONI DI SALMONI';
+  document.getElementById('textPlace').innerHTML = '';
+}
+function place005(){
+  document.getElementById('descriptionPlace').style.display = "block";
+  document.getElementById('descriptionCity').style.display = "none";
+  document.getElementById('descriptionIsland').style.display = "none";
+  document.getElementById('descriptionRoute').style.display = "none";
+  document.getElementById('titlePlace').innerHTML = 'PIANTAGIONI DI POLLI';
+  document.getElementById('textPlace').innerHTML = '';
 }
 
 /* Text description island */
 function island001(){
-  document.getElementById('description').style.display = "block";
-  document.getElementById('title').innerHTML = 'DISTRETTO RAZIONALE';
-  document.getElementById('generic').innerHTML = 'Distretto non estremista, favorevole a una crescita economica green con degli standard "eco".';
+  document.getElementById('descriptionIsland').style.display = "block";
+  document.getElementById('descriptionCity').style.display = "none";
+  document.getElementById('descriptionPlace').style.display = "none";
+  document.getElementById('descriptionRoute').style.display = "none";
+  document.getElementById('titleIsland').innerHTML = 'DISTRETTO RAZIONALE';
+  document.getElementById('textIsland').innerHTML = 'Distretto non estremista, favorevole a una crescita economica green con degli standard "eco".';
 }
 function island002(){
-  document.getElementById('description').style.display = "block";
-  document.getElementById('title').innerHTML = 'DISTRETTO ESTREMISTA';
-  document.getElementById('generic').innerHTML = 'Terra di quei vandali criminali degli ambientalisti.';
+  document.getElementById('descriptionIsland').style.display = "block";
+  document.getElementById('descriptionCity').style.display = "none";
+  document.getElementById('descriptionPlace').style.display = "none";
+  document.getElementById('descriptionRoute').style.display = "none";
+  document.getElementById('titleIsland').innerHTML = 'DISTRETTO ESTREMISTA';
+  document.getElementById('textIsland').innerHTML = 'Terra di quei vandali criminali degli ambientalisti.';
 }
 function island003(){
-  document.getElementById('description').style.display = "block";
-  document.getElementById('title').innerHTML = 'ISOLA DESERTA';
-  document.getElementById('generic').innerHTML = 'Sugli alberi crescono solo bistecche.';
+  document.getElementById('descriptionIsland').style.display = "block";
+  document.getElementById('descriptionCity').style.display = "none";
+  document.getElementById('descriptionPlace').style.display = "none";
+  document.getElementById('descriptionRoute').style.display = "none";
+  document.getElementById('titleIsland').innerHTML = 'ISOLA DESERTA';
+  document.getElementById('textIsland').innerHTML = 'Sugli alberi crescono solo bistecche.';
 }
